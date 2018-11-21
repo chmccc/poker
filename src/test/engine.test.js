c9519434a0632942a9adcf0c5971c96f029a04df
Colin McCarthy <colinmccarthy@Colin-Ms-MacBook-Pro.local>
2018-11-20 21:52:35 -0800

    initial scoring engine and tests for engine

---
+++ src/test/engine.test.js
@@ -0,0 +1,251 @@
+import { Card } from '../util/deck';
+import { getScore, getScoreObject, scoreHoleCards } from '../util/engine.js';
+
+// helper function to build card arrays with shorthand input
+const createHand = (vals, suits) => {
+  suits.forEach((char, i) => suits[i] = char.toUpperCase());
+  const suitNames = { S: 'Spades', D: 'Diamonds', C: 'Clubs', H: 'Hearts' };
+  const output = [];
+  for (let i = 0; i < vals.length; i += 1) {
+    output.push(new Card(vals[i], suitNames[suits[i]]));
+  }
+  return output;
+}
+
+describe('getScoreObject tests', () => {
+  let testScoreObj = getScoreObject(createHand([14,12,13,11,10],['s','s','s','s','s']));
+
+  test('should throw error if not provided an array of correct size', () => {
+    expect(() => getScoreObject(createHand([2,3,4,5,6,7,8], ['c','c','c','c','c','c','c']))).toThrow();
+  }) 
+  
+  test('should return a score object from a valid hand', () => {
+    expect(testScoreObj).toMatchObject({
+      score: expect.any(Number),
+      type: expect.any(String),
+      highHandCards: expect.any(Array),
+      cardsUsed: expect.any(Array)
+    });
+  });
+
+  test('score object should contain array of cards used', () => {
+    expect(testScoreObj.cardsUsed).toEqual(expect.arrayContaining([new Card(10, 'Spades')]));
+    expect(testScoreObj.cardsUsed.length).toEqual(5);
+  });
+
+  test('should properly analyze a straight flush', () => {
+    let sFTestScoreObj = getScoreObject(createHand([14,12,13,11,10],['s','s','s','s','s']));
+    expect(sFTestScoreObj.score).toEqual(8);
+    expect(sFTestScoreObj.highHandCards).toEqual(expect.arrayContaining([new Card(14, 'Spades')]));
+    expect(sFTestScoreObj.highHandCards).toHaveLength(1);
+    expect(sFTestScoreObj.type).toEqual('a straight flush');
+    expect(sFTestScoreObj.cardsUsed).toHaveLength(5);
+    sFTestScoreObj = getScoreObject(createHand([3,4,5,6,7], ['h','h','h','h','h']));
+    expect(sFTestScoreObj.score).toEqual(8);
+    // ensure low ace is not counted as the high card in a straight
+    sFTestScoreObj = getScoreObject(createHand([2,3,4,5,14], ['h','h','h','h','h']));
+    expect(sFTestScoreObj.highHandCards).toEqual(expect.arrayContaining([new Card(5, 'Hearts')]));
+    expect(sFTestScoreObj.highHandCards).toHaveLength(1);
+    expect(sFTestScoreObj.highHandCards[0].value).toEqual(5);
+  });
+
+  test('should recognize an ace has value 1 or 14', () => {
+    let aceTestScoreObj = getScoreObject(createHand([14,2,3,4,5],['s','s','s','s','s']));
+    expect(aceTestScoreObj.score).toEqual(8);
+    aceTestScoreObj = getScoreObject(createHand([14,12,13,11,10],['s','s','s','s','s']));
+    expect(aceTestScoreObj.score).toEqual(8);
+  });
+
+  test('should properly analyze four of a kind', () => {
+    const fourTestScoreObj = getScoreObject(createHand([4,4,4,4,1], ['h','d','c','s','h']));
+    expect(fourTestScoreObj.score).toEqual(7);
+    expect(fourTestScoreObj.highHandCards).toEqual(expect.arrayContaining([new Card(4, 'Hearts')]));
+    expect(fourTestScoreObj.highHandCards).toHaveLength(1);
+    expect(fourTestScoreObj.type).toEqual('four of a kind');
+    expect(fourTestScoreObj.cardsUsed).not.toEqual(expect.arrayContaining([new Card(1, 'Hearts')])); // the unused card should not be in cardsUsed
+  });
+
+  test('should properly analyze a full house', () => {
+    let fullTestScoreObj = getScoreObject(createHand([12,13,12,13,12], ['s','c','d','h','s']));
+    expect(fullTestScoreObj.score).toEqual(6);
+    expect(fullTestScoreObj.highHandCards[0].value).toEqual(12);
+    expect(fullTestScoreObj.highHandCards).toHaveLength(1);
+    expect(fullTestScoreObj.type).toEqual('a full house');
+    expect(fullTestScoreObj.cardsUsed).toHaveLength(5);
+    fullTestScoreObj = getScoreObject(createHand([3,3,3,2,2], ['s','c','d','h','s']));
+    expect(fullTestScoreObj.score).toEqual(6);
+  });
+
+  test('should properly analyze a straight', () => {
+    let straightTestScoreObj = getScoreObject(createHand([9,8,7,6,5], ['s','c','d','h','s']));
+    expect(straightTestScoreObj.score).toEqual(5);
+    expect(straightTestScoreObj.highHandCards).toEqual(expect.arrayContaining([new Card(9, 'Spades')]));
+    expect(straightTestScoreObj.highHandCards).toHaveLength(1);
+    expect(straightTestScoreObj.type).toEqual('a straight');
+    expect(straightTestScoreObj.cardsUsed).toHaveLength(5);
+    straightTestScoreObj = getScoreObject(createHand([2,3,4,5,14], ['s','c','d','h','s']));
+    expect(straightTestScoreObj.score).toEqual(5);
+  });
+
+  test('should properly analyze a flush', () => {
+    let flushTestScoreObj = getScoreObject(createHand([9,10,7,4,5], ['c','c','c','c','c']));
+    expect(flushTestScoreObj.score).toEqual(4);
+    expect(flushTestScoreObj.highHandCards).toEqual(expect.arrayContaining([new Card(9, 'Clubs')]));
+    expect(flushTestScoreObj.highHandCards).toHaveLength(5);
+    expect(flushTestScoreObj.highHandCards[0]).toEqual(new Card(10, 'Clubs'));
+    expect(flushTestScoreObj.highHandCards[4]).toEqual(new Card(4, 'Clubs'));
+    expect(flushTestScoreObj.type).toEqual('a flush');
+    expect(flushTestScoreObj.cardsUsed).toHaveLength(5);
+    flushTestScoreObj = getScoreObject(createHand([11,12,13,14,4], ['h','h','h','h','h']));
+    expect(flushTestScoreObj.score).toEqual(4);
+  });
+
+  test('should properly analyze three of a kind', () => {
+    let threeTestScoreObj = getScoreObject(createHand([9,9,9,4,5], ['c','h','s','c','c']));
+    expect(threeTestScoreObj.score).toEqual(3);
+    expect(threeTestScoreObj.highHandCards).toEqual(expect.arrayContaining([new Card(9, 'Clubs')]));
+    expect(threeTestScoreObj.highHandCards).toHaveLength(1);
+    expect(threeTestScoreObj.type).toEqual('three of a kind');
+    expect(threeTestScoreObj.cardsUsed).toHaveLength(3);
+    expect(threeTestScoreObj.cardsUsed).not.toEqual(expect.arrayContaining([new Card(4, 'Clubs'), new Card(5, 'Clubs')])); // the unused cards should not be in cardsUsed
+    threeTestScoreObj = getScoreObject(createHand([4,2,12,12,12], ['h','c','s','d','c']));
+    expect(threeTestScoreObj.score).toEqual(3);
+  });
+
+  test('should properly analyze two pair', () => {
+    let twoPairTestScoreObj = getScoreObject(createHand([8,8,4,4,5], ['c','h','s','c','c']));
+    expect(twoPairTestScoreObj.score).toEqual(2);
+    expect(twoPairTestScoreObj.highHandCards[0].value).toEqual(8);
+    expect(twoPairTestScoreObj.highHandCards[1].value).toEqual(4);
+    expect(twoPairTestScoreObj.highHandCards).toHaveLength(2);
+    expect(twoPairTestScoreObj.type).toEqual('two pair');
+    expect(twoPairTestScoreObj.cardsUsed).toHaveLength(4);
+    expect(twoPairTestScoreObj.cardsUsed).not.toEqual(expect.arrayContaining([new Card(5, 'Clubs')])); // the unused card should not be in cardsUsed
+    twoPairTestScoreObj = getScoreObject(createHand([3,2,11,11,3], ['h','c','s','d','c']));
+    expect(twoPairTestScoreObj.score).toEqual(2);
+  });
+
+  test('should properly analyze a pair', () => {
+    let pairTestScoreObj = getScoreObject(createHand([6,6,3,4,5], ['c','h','s','c','c']));
+    expect(pairTestScoreObj.score).toEqual(1);
+    expect(pairTestScoreObj.highHandCards[0].value).toEqual(6);
+    expect(pairTestScoreObj.highHandCards).toHaveLength(1);
+    expect(pairTestScoreObj.type).toEqual('a pair');
+    expect(pairTestScoreObj.cardsUsed).toHaveLength(2);
+    expect(pairTestScoreObj.cardsUsed).not.toEqual(expect.arrayContaining([new Card(3, 'Spades'), new Card(4, 'Clubs'), new Card(5, 'Clubs')])); // the unused card should not be in cardsUsed
+    pairTestScoreObj = getScoreObject(createHand([11,14,2,7,14], ['h','c','s','d','c']));
+    expect(pairTestScoreObj.score).toEqual(1);
+  });
+
+  test('should return a 0-score object on a hand with high card only', () => {
+    let highCardTestScoreObj = getScoreObject(createHand([6,9,3,4,5], ['c','h','s','c','c']));
+    expect(highCardTestScoreObj.score).toEqual(0)
+    expect(highCardTestScoreObj.highHandCards[0].value).toEqual(9);
+    expect(highCardTestScoreObj.highHandCards).toHaveLength(5);
+    expect(highCardTestScoreObj.type).toEqual('a high card');
+    expect(highCardTestScoreObj.cardsUsed).toHaveLength(1);
+    highCardTestScoreObj = getScoreObject(createHand([11,14,3,7,12], ['h','c','s','d','c']));
+    expect(highCardTestScoreObj.score).toEqual(0);
+  });
+
+});
+
+describe('scoreHoleCards tests: ', () => {
+  let testScoreObj = scoreHoleCards([new Card(2, 'Spades'), new Card (2, 'Diamonds')]);
+  
+  test('should throw error if not provided an array of correct size', () => {
+    expect(() => scoreHoleCards([new Card(14, 'Spades')])).toThrow();
+  }) 
+  
+  test('should return a score object from a valid hole hand', () => {
+    expect(testScoreObj).toMatchObject({
+      score: expect.any(Number),
+      cards: expect.any(Array)
+    });
+  });
+
+  test('should detect a pair', () => {
+    expect(testScoreObj.cards).toHaveLength(2);
+    expect(testScoreObj.cards[0].value).toEqual(2);
+    expect(testScoreObj.cards[1].value).toEqual(2);
+    expect(testScoreObj.score).toEqual(.02);
+  });
+
+  test('should return a sorted array of cards with proper fractional score based on high hole card', () => {
+    testScoreObj = scoreHoleCards([new Card(4, 'Spades'), new Card (13, 'Diamonds')]);
+    expect(testScoreObj.cards[0].value).toEqual(13);
+    expect(testScoreObj.score).toEqual(0.013);
+  });
+
+});
+
+describe('getScore basic tests: ', () => {
+  let testScoreObj = getScore(createHand([11,2], ['h','d']), createHand([3,4,5,6,9], ['d','d','s','c','d']), 'test');
+
+  test('should return a score object from an array of 2 cards and an array of 5 cards', () => {
+    expect(testScoreObj.score).toEqual(5);
+    expect(testScoreObj.highHandCards[0].value).toEqual(6);
+    expect(testScoreObj.cardsUsed).toEqual(expect.arrayContaining([new Card(6, 'Clubs'), new Card(5, 'Spades'), new Card(4, 'Diamonds'), new Card(3, 'Diamonds'), new Card(2, 'Diamonds')]));
+  });
+
+  test('should correctly identify the high hole card(s)', () => {
+    testScoreObj = getScore(createHand([1,3], ['s','d']), createHand([3,3,1,14,9], ['d','d','s','c','d']), 'test'); // full house, 3s over aces, ace high in hole
+    expect(testScoreObj.score).toEqual(6);
+    expect(testScoreObj.holeCardsScore).toEqual(0.014);
+    testScoreObj = getScore(createHand([7,7], ['s','c']), createHand([8,9,10,11,13], ['d','c','d','s','d']), 'test'); // straight, 7 - jack, pair of 7s in hole
+    expect(testScoreObj.score).toEqual(5);
+    expect(testScoreObj.holeCardsScore).toEqual(.07);
+  });
+  
+});
+
+describe('comprehensive scoring tests for getScore:', () => {
+  let testScoreObj = getScore(createHand([4,4], ['s','s']), createHand([4,4,3,3,3], ['s','c','s','c','d']), 'test');
+  
+  test('should choose 4 of a kind over 3 of a kind or full house', () => {
+    expect(testScoreObj.score).toEqual(7);
+  });
+
+  test('should choose the best 2 of 3 pairs', () => {
+    testScoreObj = getScore(createHand([12,12], ['s','d']), createHand([2,11,2,7,7], ['s','c','h','c','d']), 'test');
+    expect(testScoreObj.score).toEqual(2);
+    expect(testScoreObj.highHandCards).toHaveLength(2);
+    expect(testScoreObj.highHandCards[0].value).toEqual(12);
+    expect(testScoreObj.highHandCards[1].value).toEqual(7);
+    expect(testScoreObj.highHoleCards[0].value).toEqual(12);
+  });
+
+  test('should choose the highest straight', () => {
+    testScoreObj = getScore(createHand([2,3], ['s','d']), createHand([4,5,6,7,8], ['s','c','h','c','d']), 'test');
+    expect(testScoreObj.score).toEqual(5);
+    expect(testScoreObj.highHandCards).toHaveLength(1);
+    expect(testScoreObj.highHandCards[0].value).toEqual(8);
+    expect(testScoreObj.highHoleCards[0].value).toEqual(3);
+
+  });
+
+  test('should choose a straight over a flush', () => {
+    testScoreObj = getScore(createHand([14,3], ['s','s']), createHand([4,5,6,7,9], ['s','d','s','s','s']), 'test');
+    expect(testScoreObj.score).toEqual(5);
+    expect(testScoreObj.highHandCards).toHaveLength(1);
+    expect(testScoreObj.highHandCards[0].value).toEqual(7);
+    expect(testScoreObj.highHoleCards[0].value).toEqual(14);
+  });
+
+  test('should choose a flush over 3 of a kind', () => {
+    testScoreObj = getScore(createHand([11,3], ['s','d']), createHand([3,3,12,7,4], ['s','h','s','s','s']), 'test');
+    expect(testScoreObj.score).toEqual(4);
+    expect(testScoreObj.highHandCards).toHaveLength(5);
+    expect(testScoreObj.highHandCards[0].value).toEqual(12);
+    expect(testScoreObj.highHoleCards[0].value).toEqual(11);
+  });
+  
+  test('should choose a straight flush over a higher-number straight', () => {
+    testScoreObj = getScore(createHand([13,8], ['s','d']), createHand([13,12,11,10,9], ['h','d','d','d','d']), 'test');
+    expect(testScoreObj.score).toEqual(8);
+    expect(testScoreObj.highHandCards).toHaveLength(1);
+    expect(testScoreObj.highHandCards[0].value).toEqual(12);
+    expect(testScoreObj.highHoleCards[0].value).toEqual(13);
+  });
+
+});
---
+++ src/util/engine.js
@@ -0,0 +1,167 @@
+// caches all hands passed in regardless of size -- should improve performance in any function that looks for a best score from given cards
+class ScoreCache {
+  constructor() {
+    this.cache = {};
+    this.hits = 0;
+  }
+
+  checkCache(hand) {
+    const string = JSON.stringify(hand);
+    if (this.cache[string]) {
+      this.hits++;
+      return [this.cache[string], string];
+    }
+    // console.log('cache hits: ', this.hits);
+    return [null, string];
+  }
+
+  addToCache(string, scoreObject) {
+    this.cache[string] = scoreObject;
+    return true;
+  }
+
+  clearCache() { this.cache = {}; this.hits = 0; }
+
+}
+
+let cache = new ScoreCache();
+
+// helper function to build a score object from a hand
+const getScoreObject = (hand) => {
+  const [cached, cacheString] = cache.checkCache(hand);
+  if (cached) return cached;
+  hand = hand.sort((a, b) => a.value - b.value);
+  if (hand.length !== 5) throw new Error('getScoreObject: hand size must be 5!');
+  
+  // we'll detect straight and flush during loop 
+  let hasStraight = true;
+  let hasFlush = true;
+  let scoreObject = { score: 0, type: 'a high card' }; // base score object
+  const cardsByValue = {};
+
+  // build an object to store how many of each card the hand has, and update hasStraight/hasFlush
+  for (let i = 0; i < hand.length; i += 1) {
+    const card = hand[i];
+    if (i !== 0 && card.value !== hand[i - 1].value + 1) {
+      if (hasStraight && card.isAce && hand[0].value === 2) hasStraight = true; // back to true if hand is 2 3 4 5 A
+      else hasStraight = false;
+    }
+    if (i !== 0 && card.suit !== hand[i - 1].suit) hasFlush = false;
+    if (!cardsByValue[card.value]) cardsByValue[card.value] = [];
+    cardsByValue[card.value].push(card);
+  }
+
+  // swap out of place ace in an ace-low straight
+  if (hasStraight && hand[4].isAce && hand[0].value === 2) {
+    hand.unshift(hand[4]);
+    hand.splice(5, 1);
+  }
+
+  if (hasStraight && hasFlush) scoreObject = { score: 8, type: 'a straight flush', cardsUsed: hand, highHandCards: [hand[4]] };
+  else if (hasStraight) scoreObject = { score: 5, type: 'a straight', cardsUsed: hand, highHandCards: [hand[4]] };
+  else if (hasFlush) scoreObject = { score: 4, type: 'a flush', cardsUsed: hand, highHandCards: hand.sort((a, b) => b.value - a.value) };
+
+  // no straight, flush, or straight flush, now we dig through cardsByValue for pairs/triplets
+  // breaks when no better hand is possible
+  else {
+    let firstPair, threeOfAKind;
+    for (const cardValue in cardsByValue) {
+      const cardSet = cardsByValue[cardValue];
+      if (cardSet.length === 4) {
+        scoreObject = { score: 7, type: 'four of a kind', cardsUsed: cardSet, highHandCards: [cardSet[0]] };
+        break;
+      }
+      if (cardSet.length === 3) { // check for 3 of a kind/full house
+        if (firstPair) {
+          scoreObject = { score: 6, type: 'a full house', cardsUsed: hand, highHandCards: [cardSet[0]]};
+          break;
+        }
+        else {
+          threeOfAKind = cardSet;
+          scoreObject = { score: 3, type: 'three of a kind', cardsUsed: cardSet, highHandCards: [cardSet[0]]};
+        }
+      }
+      if (cardSet.length === 2) { // check for pair/full house
+        if (threeOfAKind) {// will also serve as cardsUsed
+          scoreObject = { score: 6, type: 'a full house', cardsUsed: hand, highHandCards: [threeOfAKind[0]]};
+          break;
+        }
+        else if (firstPair) {
+          const sorted = [...firstPair, ...cardSet].sort((a, b) => b.value - a.value); // will also serve as cardsUsed
+          scoreObject = { score: 2, type: 'two pair', cardsUsed: sorted, highHandCards: [sorted[0], sorted[2]] };
+          break;
+        }
+        else {
+          firstPair = cardSet;
+          scoreObject = { score: 1, type: 'a pair', cardsUsed: cardSet, highHandCards: [cardSet[0]] };
+        }
+      }
+    }
+  }
+  // high card only
+  if (scoreObject.score === 0) {
+    scoreObject.highHandCards = hand.sort((a, b) => b.value - a.value);
+    scoreObject.cardsUsed = [hand[0]];
+  }
+  cache.addToCache(cacheString, scoreObject);
+  return scoreObject;
+};
+
+const compareByHighHandCards = (oldScoreObject, newScoreObject) => {
+  // console.log('both had type ' + oldScoreObject.type + ', must compare by hand cards');
+  // console.log('old best object: ', JSON.stringify(oldScoreObject.highHandCards.map(e => e.displayName)));
+  // console.log('new object: ', JSON.stringify(newScoreObject.highHandCards.map(e => e.displayName)));
+  for (let i = 0; i < newScoreObject.highHandCards.length; i++) {
+    if (newScoreObject.highHandCards[i].value > oldScoreObject.highHandCards[i].value) {
+      return { draw: false, bestScoreObject: newScoreObject };
+    }
+    if (newScoreObject.highHandCards[i].value < oldScoreObject.highHandCards[i].value) {
+      return {draw: false, bestScoreObject: oldScoreObject };
+    }
+  }
+  return { draw: true, bestScoreObject: oldScoreObject };
+}
+
+const scoreHoleCards = (hand) => {
+  if (hand.length !== 2) throw new Error(`scoreHoleCards requires array of length 2 and got length ${hand.length}`);
+  if (hand[0].value === hand[1].value) return { score: hand[0].value / 100, cards: hand }
+  const cards = hand.sort((a, b) => b.value - a.value);
+  return { score: cards[0].value / 1000, cards }
+}
+
+const getScoreRecursively = (hand) => {
+  const [cached, cacheString] = cache.checkCache(hand);
+  if (cached) return cached;
+  if (hand.length === 5) return getScoreObject(hand);
+  let bestScoreObject = null;
+  if (hand.length > 5) {
+    for (let i = 0; i < hand.length; i++) {
+      let newScoreObject;
+      if (hand.length === 7) {
+        newScoreObject = getScoreRecursively([...hand.slice(0, i), ...hand.slice(i + 1)])
+      } else newScoreObject = getScoreObject([...hand.slice(0, i), ...hand.slice(i + 1)]);
+      if (!bestScoreObject) bestScoreObject = newScoreObject;
+      else if (newScoreObject.score > bestScoreObject.score) bestScoreObject = newScoreObject;
+      else if (newScoreObject.score === bestScoreObject.score) {
+        // will use draw boolean later...
+        const { bestScoreObject: bestScoreObjectByComparison, draw } = compareByHighHandCards(bestScoreObject, newScoreObject);
+        bestScoreObject = bestScoreObjectByComparison;
+      }
+    }
+  }
+  cache.addToCache(cacheString, bestScoreObject);
+  return bestScoreObject;
+}
+
+const getScore = (holeCards, tableCards, owner) => {
+  const fullHand = [...holeCards, ...tableCards].sort((a, b) => a.value - b.value);
+  const bestScoreObject = getScoreRecursively(fullHand);
+  if (owner) bestScoreObject.owner = owner;
+  const { score, cards } = scoreHoleCards(holeCards);
+  bestScoreObject.holeCardsScore = score;
+  bestScoreObject.highHoleCards = cards;
+  // console.log('cache hits so far: ', cache.hits)
+  return bestScoreObject;
+}
+
+export { getScore, getScoreObject, scoreHoleCards, ScoreCache };
