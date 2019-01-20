2ef3097d06ec98a94d1f65d4e1b3ff8d4dcba5c3
Colin McCarthy <colinmccarthy@Colin-Ms-MacBook-Pro.local>
2019-01-19 18:18:33 -0800

    changed messages display from 4 to 5 max messages onscreen

--- src/util/infoMessagesQueue.js
+++ src/util/infoMessagesQueue.js
@@ -7,18 +7,18 @@
 /** @constructor Accepts an optional array of messages to initialize with */
 export class InfoMessagesQueue {
   constructor(array) {
-    if (array && array.length > 4) throw new Error('InfoMessageQueues cannot contain more than 4 messages')
+    if (array && array.length > 5) throw new Error('InfoMessageQueues cannot contain more than 4 messages')
     this.q = array || [];
   }
 
   /**
-   * Enqueues up to 4 items (this queue class is limited to 4)
+   * Enqueues up to 5 items (this queue class is limited to 5)
    * @param {string} msg The message (or messages) to enqueue
    * @returns the updated queue instance
    */
   add = (...msgs) => {
     msgs.forEach(msg => {
-      if (this.q.length === 4) this.q.shift();
+      if (this.q.length === 5) this.q.shift();
       this.q.push(msg);
     });
     return this;
