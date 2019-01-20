f1b55e9b53d721fac074a7fcffd95f9aeb975800
Colin McCarthy <colinmccarthy@Colin-Ms-MacBook-Pro.local>
2019-01-19 18:16:44 -0800

    add styling to Options panel

--- src/components/OptionsPanel.js
+++ src/components/OptionsPanel.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 const OptionsPanel = ({ options, callbacks }) => {
 
-  const validOptions = Object.keys(options).filter(option => options[option]);
-
   return (
-    <div className="PlayerOptions">
-    {validOptions.map(option => <button
-        key={`option-${option}`}
-        className="playerOption-button" 
-        onClick={() => callbacks[option]('player')}>{`${option}`}
-      </button>
-    )}
+    <div style={{padding: '10px'}}>
+      <h3>Options</h3>
+      <div id="PlayerOptions">
+      {Object.keys(options).map(optionName => <button
+          key={`option-${optionName}`}
+          className={`playerOption-button${options[optionName] ? '' : ' button-disabled'}`}
+          onClick={() => callbacks[optionName]('player')}
+          disabled={!options[optionName]}>
+            {`${optionName}`}
+          </button>
+      )}
+      </div>
     </div>
   )
 }
