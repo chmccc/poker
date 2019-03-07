1ae1d40edde01e6a1a2dea0674fb44d572a0b6c2
Colin McCarthy <colinmccarthy@Colin-Ms-MBP.lan1>
2019-03-07 01:42:14 -0800

    add env variables compatible with CRA

---
+++ src/env.js
@@ -0,0 +1,10 @@
+/**
+ * @module env.js
+ * Workaround for create-react-app webpack config & env variables, to be used like dotenv
+ */
+
+export default {
+  AI_DEBUG_ON: false,
+  ENGINE_DEBUG_ON: false,
+  FRONTEND_DEBUG_ON: false,
+};
